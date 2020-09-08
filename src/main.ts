fa244eaba5c3a196c5312fbef1e5ff0cf5350dc5
David Kale <dakale@github.com>
2020-09-08 13:09:16 -0400

    Paginate and remove old labels (#96)
    * Paginate to get all changed files. Add option to remove labels
    
    * build
    
    * format and build

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import * as core from '@actions/core';
-import * as github from '@actions/github';
-import * as yaml from 'js-yaml';
-import {Minimatch, IMinimatch} from 'minimatch';
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+import * as yaml from "js-yaml";
+import { Minimatch, IMinimatch } from "minimatch";
 
 interface MatchConfig {
   all?: string[];
@@ -12,16 +12,23 @@ type StringOrMatchConfig = string | MatchConfig;
 
 async function run() {
   try {
-    const token = core.getInput('repo-token', {required: true});
-    const configPath = core.getInput('configuration-path', {required: true});
+    const token = core.getInput("repo-token", { required: true });
+    const configPath = core.getInput("configuration-path", { required: true });
+    const syncLabels = !!core.getInput("sync-labels", { required: false });
 
     const prNumber = getPrNumber();
     if (!prNumber) {
-      console.log('Could not get pull request number from context, exiting');
+      console.log("Could not get pull request number from context, exiting");
       return;
     }
 
     const client = new github.GitHub(token);
+
+    const { data: pullRequest } = await client.pulls.get({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      pull_number: prNumber
+    });
 
     core.debug(`fetching changed files for pr #${prNumber}`);
     const changedFiles: string[] = await getChangedFiles(client, prNumber);
@@ -31,15 +38,22 @@ async function run() {
     );
 
     const labels: string[] = [];
+    const labelsToRemove: string[] = [];
     for (const [label, globs] of labelGlobs.entries()) {
       core.debug(`processing ${label}`);
       if (checkGlobs(changedFiles, globs)) {
         labels.push(label);
+      } else if (pullRequest.labels.find(l => l.name === label)) {
+        labelsToRemove.push(label);
       }
     }
 
     if (labels.length > 0) {
       await addLabels(client, prNumber, labels);
+    }
+
+    if (syncLabels && labelsToRemove.length) {
+      await removeLabels(client, prNumber, labelsToRemove);
     }
   } catch (error) {
     core.error(error);
@@ -60,17 +74,18 @@ async function getChangedFiles(
   client: github.GitHub,
   prNumber: number
 ): Promise<string[]> {
-  const listFilesResponse = await client.pulls.listFiles({
+  const listFilesOptions = client.pulls.listFiles.endpoint.merge({
     owner: github.context.repo.owner,
     repo: github.context.repo.repo,
     pull_number: prNumber
   });
 
-  const changedFiles = listFilesResponse.data.map(f => f.filename);
+  const listFilesResponse = await client.paginate(listFilesOptions);
+  const changedFiles = listFilesResponse.map(f => f.filename);
 
-  core.debug('found changed files:');
+  core.debug("found changed files:");
   for (const file of changedFiles) {
-    core.debug('  ' + file);
+    core.debug("  " + file);
   }
 
   return changedFiles;
@@ -224,6 +239,23 @@ async function addLabels(
     issue_number: prNumber,
     labels: labels
   });
+}
+
+async function removeLabels(
+  client: github.GitHub,
+  prNumber: number,
+  labels: string[]
+) {
+  await Promise.all(
+    labels.map(label =>
+      client.issues.removeLabel({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        issue_number: prNumber,
+        name: label
+      })
+    )
+  );
 }
 
 run();
