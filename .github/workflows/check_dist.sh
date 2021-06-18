7ad42be42df74de2b89e9365c08ff678a62ae611
Patrick Ellis <patrick.j.ellis@gmail.com>
2021-06-18 13:51:07 -0400

    f

--- .github/workflows/check_dist.sh
+++ .github/workflows/check_dist.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-dist_index_diff=$(git diff --exit-code -- dist/index.js)
+dist_index_diff=$(git diff --exit-code --text -- dist/index.js)
 
 if [[ "$dist_index_diff" ]]; then
   echo -e "$dist_index_diff\n‼️  Changes detected to dist/index.js! \n\tPlease run \`npm run build' and commit the result." >&2;
