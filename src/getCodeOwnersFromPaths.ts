cf9c69585084eece43c940ac54fc7bdbcad16301
Chen Zheng <chen.zheng@li.me>
2022-11-28 14:56:15 -0800

    fix typo

--- src/getCodeOwnersFromPaths.ts
+++ src/getCodeOwnersFromPaths.ts
@@ -3,12 +3,14 @@ import Codeowners from 'codeowners'
 export async function getCodeOwnersFromPaths(
   paths: string[]
 ): Promise<string[]> {
-  const repos = new Codeowners()
-  const owners: Set<string> = new Set()
+  const repos = new Codeowners();
+  const owners: Set<string> = new Set();
+  core.debug("fetching codeowner");
   for (const path of paths) {
-    const pathowners = repos.getOwner(path)
+    const pathowners = repos.getOwner(path);
     for (const pathowner of pathowners) {
-      owners.add(pathowner)
+      owners.add(pathowner);
+      core.debug(`found codeowner: ${pathowner}`);
     }
   }
   return Array.from(owners);
